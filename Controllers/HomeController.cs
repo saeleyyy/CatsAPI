1889a2b9daae93ed2141982dda50c4ef11f4e3f4
Samuel Eley <78078726+saeleyyy@users.noreply.github.com>
2022-06-18 17:51:18 +0100

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http.Headers;
+using CatAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CatAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        HttpClient client = new HttpClient();
+        
+        public async Task<IActionResult> Index()
+        {
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            
+            HttpResponseMessage response = await client.GetAsync("https://api.thecatapi.com/v1/images/search?limit=2");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string res = await response.Content.ReadAsStringAsync();
+                List<CatModel> cat = JsonConvert.DeserializeObject<List<CatModel>>(res);
+                ViewBag.Images = cat;
+            }
+            
+            return View();
+        }
+        
+        [HttpGet]
+        public string GetImage()
+        {
+            Task<string> url = GetRandomImageURL();
+
+            return url.Result;
+        }
+
+        private async Task<string> GetRandomImageURL()
+        {
+            HttpResponseMessage response = await client.GetAsync("https://api.thecatapi.com/v1/images/search?limit=1");
+            string res = await response.Content.ReadAsStringAsync();
+
+            return res;
+        }
+    }
+}
---
+++ Models/CatModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CatAPI.Models
+{
+	public class CatModel
+	{
+		[JsonIgnore]
+		public string[] breeds { get; set; }
+		
+		public string id { get; set; }
+
+		public string url { get; set; }
+
+		public int? width { get; set; }
+
+		public int? height { get; set; }	
+	}
+}
+
