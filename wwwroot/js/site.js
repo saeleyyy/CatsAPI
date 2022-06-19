982cfe6bc4dc134eaf5b2f1fd45f29f9014f7f5d
Samuel Eley <78078726+saeleyyy@users.noreply.github.com>
2022-06-19 13:35:02 +0100

    move js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,4 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
+$(document).ready(function () {
+    $('img').click(function () {
+        $(this).addClass('animate__animated animate__shakeY');
+        let change = $(this);
+        $.ajax({
+            type: "GET",
+            url: "@Url.Action("GetImage")",
+            dataType: "JSON",
+            async: false,
+            success: function (result) {
+                if ($(change).attr('id') == 'img-one') {
+                    $('#img-two').attr('src', result[0]['url'])
+                } else {
+                    $('#img-one').attr('src', result[0]['url'])
+                }
+            },
+            error: function(err) {
+                console.log(err)
+            }
+        })
+        $(this).on('animationend', function () {
+            $(this).removeClass('animate__animated animate__shakeY');
+        } )
+    });
+})
